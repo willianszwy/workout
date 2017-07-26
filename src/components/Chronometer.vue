f761e50f493dc1444e3c2a7883f1ed356cfe641a
willianszwy <willianszwy@gmail.com>
2017-07-25 23:20:53 -0400

    add chronometer comp

---
+++ src/components/Chronometer.vue
@@ -0,0 +1,35 @@
+<template>
+ 
+      <div class="chronometer">
+          <h2>{{ chronometer.action }}</h2> 
+
+          <p>{{ chronometer.display }}</p>
+          <h2>{{chronometer.counter}}/{{repetition}}</h2>
+    </div>  
+  
+    
+</template>
+
+<script>
+
+import { mapState } from 'vuex'
+
+export default {
+  name: 'hello',
+  computed: {
+    ...mapState([
+      'repetition',
+      'chronometer'
+    ])
+  }
+}
+</script>
+
+<style>
+
+.chronometer {
+    font-size: 7em;
+    text-align: center;
+ }
+    
+</style>
